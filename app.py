dd3a2b4f699f76debc3795ea5701c0b9b5682b94
QuratulainCoder <quratulainkhan3330@gmail.com>
2025-09-28 15:48:43 +0500

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from chatbott import SimpleChatbot
+from chatbott import UniversityChatbot
 import os
 
 app = Flask(__name__)
 CORS(app)
-chatbot = SimpleChatbot()
+chatbot = UniversityChatbot()
 
 @app.route('/')
 def home():
